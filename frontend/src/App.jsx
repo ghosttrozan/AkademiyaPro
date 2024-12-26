ead4a223fbe6fef7e6be04f4a4d0bf243cc17a22
Alkaif <alkaifkhawarizmi@gmail.com>
2024-12-26 15:19:58 +0530

    ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PrincipalSignUp from './pages/PrincipalSignUp'
+import Dashboard from './Dashboard';
+import PrincipalSignIn from './pages/PrincipalSignIn';
 
 function App() {
   return (
-    <div className=''>
-      <PrincipalSignUp />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Dashboard/>} />
+        <Route path='/signup' element={<PrincipalSignUp />} />
+        <Route path='/dashboard' element={<PrincipalSignIn />} />
+      </Routes>
+    </Router>
   )
 }
 
